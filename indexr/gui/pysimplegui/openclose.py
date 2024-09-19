e6d10274d873a6c054670b50542b0aacaafc8b2e
Jay Glickman <jayglickman@Jays-MacBook-Pro.local>
2024-09-18 21:29:40 -0400

    pysimplegui updates

--- indexr/gui/pysimplegui/openclose.py
+++ indexr/gui/pysimplegui/openclose.py
@@ -33,12 +33,41 @@ def load_image_from_path(file_path, resize_x=PREVIEW_IMG_WIDTH, resize_y=PREVIEW
         return image_bytes
 
 
+def update_tag_button(index, tag):
+    # tab = sg.Col(
+    #     [
+    #         sg.B("X", border_width=0, button_color=(sg.theme_text_color(), sg.theme_background_color()),
+    #         k=('-DEL-', tag['id']), tooltip='Delete this item'),
+    #         sg.B(tag['tag_name'], size=(10, 1), k=('-DESC-', tag['id'])),
+    #     ]
+    # )
+
+    tab = [sg.pin(sg.Col([[sg.B("X", border_width=0, pad=0, button_color=(sg.theme_text_color(), sg.theme_background_color()), k=(f'{index}-X', 5), tooltip='Delete this item'),
+                            sg.B(f'{tag.get("tag_name")}', button_color=("white", "gray"), pad=0, k=('-STATUS-', 5))]], k=(f'{index}', 5)))]
+    return tab
+
+
+tag_count = 10
+tags_row = [sg.Button("tags", key="-TAGS-")]
+for tag_button_index in range(tag_count):
+    tags_row.append(sg.Button(f"{tag_button_index}", key=tag_button_index, visible=False))
+tags_row.append(sg.Button("update tags", key="-UPDATE-TAGS-"))
+tag_col = [
+    sg.Column(
+        [
+            tags_row
+        ],
+        expand_x=True
+    )
+]
+
 layout = [
     [sg.Text("What's your name?")],
     [sg.Text(size=(40, 1), key="-OUTPUT-")],
     [sg.Button("Random Image"), sg.Button("Quit")],
     [sg.Image(key="-PREVIEW-", size=(PREVIEW_IMG_WIDTH, PREVIEW_IMG_HEIGHT))],
-    [sg.Input(key="-TAGS-"), sg.Button(key="-UPDATE-TAGS-")],
+    [sg.Input(size=(40, 1), key="-NEW-TAGS-")],
+    tag_col,
     [sg.Button(size=(40, 1), key="-IMG_NAME-")],
     [sg.Button(size=(100, 2), key="-IMG_DIR-")]
 ]
@@ -52,6 +81,8 @@ random_row = {
     "id": None
 }
 query_class = TableQueries()
+tag_buttons = []
+image_tags = []
 # Display and interact with the Window using an Event Loop
 while True:
     event, values = window.read()
@@ -66,8 +97,8 @@ while True:
         if img_directory:
             subprocess.call(['open', img_directory])
     elif event == "-UPDATE-TAGS-":
-        tags = values["-TAGS-"].split(" ") if values["-TAGS-"] else []
-        print("tags are", tags)
+        tags = values["-NEW-TAGS-"].split(" ") if values["-NEW-TAGS-"] else []
+        print("added tags are", tags)
 
         created_tag_ids = []
         for tag in tags:
@@ -85,40 +116,43 @@ while True:
             print("assign_tag_to_file", res)
     # Output a message to the window
     elif event == "Random Image":
+        image_tags = []
         random_row = query_class.get_random_image_ref()
         if not random_row:
             continue
         image_tags = query_class.get_tags_for_image_id(random_row["id"])
+        # for index, tag in enumerate(image_tags):
+        #     tag_buttons.append(update_tag_button(index, tag))
         image_tag_names = get_tag_names_from_dict(image_tags)
         print("image tags", image_tags, image_tag_names)
         print("random row file--->", random_row)
         img_path = random_row.get("file")
         img_directory, img_name = split_path_and_file(img_path)
         preview_img = load_image_from_path(img_path)
-        print("type-->", type(preview_img))
-        print("image--->", preview_img)
+        print("tag_buttons", tag_buttons)
         layout = [
             [sg.Text("Behold!")],
             [sg.Text(size=(40, 1), key="-OUTPUT-")],
             [sg.Button("Random Image"), sg.Button("Quit")],
             [sg.Image(preview_img, key='-IMAGE-')],
-            [sg.Input(key="-TAGS-"), sg.Button(key="-UPDATE-TAGS-")],
+            [sg.Input(size=(25, 1), key="-NEW-TAGS-")],
+            tag_col,
             [sg.Button("Load Image", size=(40, 1), key="-IMG_NAME-")],
             [sg.Text(size=(40, 1), key="-IMG_DIR-")]
         ]
         window["-PREVIEW-"].update(
             data=preview_img.getvalue()
         )
-        print("image path", img_path)
-        print("image dire", img_directory)
         window["-IMG_NAME-"].update(img_name)
         window["-IMG_DIR-"].update(img_directory)
         window["-UPDATE-TAGS-"].update("Update Tags")
-        window["-TAGS-"].update(' '.join(image_tag_names))
-
-    # window["-OUTPUT-"].update(
-    #     "Hello " + values["-TAGS-"] + "! Thanks for trying PySimpleGUI"
-    # )
+        # cleanup old tags before showing new:
+        for index in range(tag_count):
+            window[index].update(visible=False)
+        for index, tag in enumerate(image_tags):
+            window[index].update(tag.get("tag_name"), visible=True)
+        for tag_button in tag_buttons:
+            window.extend_layout(window['-TAGS-'], [tag_button])
 
 # Finish up by removing from the screen
 window.close()
